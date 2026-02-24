af2eff9a94346fd700331e97300890882d790a09
unknown <a99701841@gmail.com>
2026-02-24 09:41:38 +0900

    update

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import './Editor.css'
 import Button from './Button'
 import EmotionItem from './EmotionItem'
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom'
 import { getStringedDate } from '../util/getStringedDate'
 import { emotionList } from '../util/constants'
 
-const Editor = ({onSumit}) => {
+const Editor = ({initData,onSubmit}) => {
 
   const nav = useNavigate()
   
@@ -15,6 +15,16 @@ const Editor = ({onSumit}) => {
     emotionId:3,
     content:''
   })
+
+
+  useEffect(()=>{
+    if(initData) {
+      setInput({
+        ...initData,
+        createdDate:new Date(Number(initData.createdDate))
+      })
+    }
+  },[initData])
 
   const onChangeInput = (e)=>{
     let name = e.target.name
@@ -30,8 +40,8 @@ const Editor = ({onSumit}) => {
     })
   }
 
-  const onSumitButtonClick =()=>{
-    onSumit(input)
+  const onSubmitButtonClick =()=>{
+    onSubmit(input)
   }
 
 
@@ -80,7 +90,7 @@ const Editor = ({onSumit}) => {
         />
         <Button text={'작성완료'} 
         type={'POSITIVE'}
-        onClick={onSumitButtonClick}
+        onClick={onSubmitButtonClick}
         />
       </section>
     </div>
