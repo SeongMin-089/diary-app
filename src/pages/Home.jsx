7103a9648c8e5c6c6cb838888bc5ab8e04e8b3a8
unknown <a99701841@gmail.com>
2026-02-25 11:28:50 +0900

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Header from '../components/Header'
 import Button from '../components/Button'
 import DiaryList from '../components/DiaryList'
 import { DiaryStateContext } from '../App'
+import useTitle from "../hooks/useTitle";
+
 const Home = () => {
   const data =useContext(DiaryStateContext)
   const [pivotDate, setPivotDate]=useState(new Date())
